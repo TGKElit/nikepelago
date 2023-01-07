ba5fa8e30bbb470af54c2d611c501f6a3706df26
TGKElit <tgkelit@gmail.com>
2023-01-07 20:15:13 +0100

    Started coding landing page

---
+++ src/header.php
@@ -0,0 +1,9 @@
+<header>
+    <img src="../resources/svg/star.svg"></img>
+    <h1>Itsakon Hotel</h1>
+    <section class=stars>
+        <?php for ($i=0; $i < $stars; $i++) { 
+            ?><img src="../resources/svg/star.svg"></img><?php
+        }?>
+    </section>
+</header>
--- src/index.php
+++ src/index.php
@@ -1,4 +1,31 @@
 <?php
-    echo "hello world";
+    require('general.php');
 ?>
 
+<!DOCTYPE html>
+<html lang ="eng">
+    <head>
+        <link rel="stylesheet" href="general.css">
+        <link rel="stylesheet" href="header.css">
+        <link rel="stylesheet" href="hero.css">
+        <link rel="stylesheet" href="rooms.css">
+        <title>Itsakon Hotel</title>
+    </head>
+    <body>
+        <?php require('header.php')?>
+
+        <section class="hero">
+            <img src="../resources/images/hero.png" alt="View of the Island">
+        </section>
+        <section class="rooms">
+            <h2>Our Rooms</h2>
+            <div class="room-container">
+                <article class=room id="budget"></article>
+                <article class=room id="standard"></article>
+                <article class=room id="luxury"></article>
+            </div>
+        </section>
+
+        <script src="main.js"></script>
+    </body>
+</html>
