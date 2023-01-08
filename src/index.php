d9bd4bd7efe7ef02ed929dce004e6e794f13e1f6
TGKElit <tgkelit@gmail.com>
2023-01-08 18:22:17 +0100

    updated room cards

--- src/index.php
+++ src/index.php
@@ -20,7 +20,34 @@
         <section class="rooms">
             <h2>Our Rooms</h2>
             <div class="room-container">
-                <article class=room id="budget"></article>
+                <article class=room id="budget">
+                    <h3>Budget</h3>
+                    <section class="carousell">
+                        <button class="scroll-button left" type="button">
+                            <img src="../resources/svg/left-arrow.svg" alt="Previous image">
+                        </button>
+                        <div class="images">
+                            <div class="image-container" id="budget-scroller">
+                                <img src="../resources/images/budget-one.jpg" alt="First">
+                                <img src="../resources/images/budget-two.jpg" alt="Second">
+                                <img src="../resources/images/budget-three.jpg" alt="Last">
+                            </div>
+                        </div>
+                        <button class="scroll-button right" id="budget-right" type="button">
+                            <img src="../resources/svg/right-arrow.svg" alt="Next image">
+                        </button>
+                    </section>
+                    <a href="./room/budget.php">Book Now</a>
+                    <h4>$4/day</h4>
+                    <p>5% discount per 3 days booked!</p>
+                    <section class="calendar-section">
+                        <h4>Availability</h4>
+                        <div class="calendar">
+                            <h5>January 2023</h5>
+                            <div class="days"></div>
+                        </div>
+                    </section>
+                </article>
                 <article class=room id="standard"></article>
                 <article class=room id="luxury"></article>
             </div>
---
+++ src/main.js
@@ -0,0 +1,3 @@
+document.getElementById("budget-right").addEventListener("click", function() {
+    document.getElementById("budget-scroller").style.transform = "translate(-340px)";
+});
