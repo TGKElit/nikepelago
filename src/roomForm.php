29aacf7d28a0880c18033ab1e853a243aa9095ce
TGKElit <tgkelit@gmail.com>
2023-01-10 20:44:48 +0100

    started implenting features

--- src/roomForm.php
+++ src/roomForm.php
@@ -2,7 +2,18 @@
             <input type="hidden" name="price" id="form-price" value="0">
             <input type="hidden" name="selected-dates" id="selected-dates" value="">
             <section class="features">
-    
+                <h4>Features</h4>
+                <div></div>
+                <?php
+                for ($i=0; $i < 28; $i++) { 
+                    ?>
+                    <div class="feature">
+                        <input type="checkbox" name="feature<?=$i?>">
+                        <label for="feature<?=$i?>">A feature! $2</label>
+                    </div>
+                    <?php
+                }
+                ?>
             </section>
             <section class="complete-purchace">
                 <input type="text" name="transfer-code">
