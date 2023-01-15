9ec59c9c9ab534f9a2c5429154e5f44864dbc937
TGKElit <tgkelit@gmail.com>
2023-01-15 22:24:41 +0100

    logo path

--- src/header.php
+++ src/header.php
@@ -1,8 +1,8 @@
 <header>
-    <img src="../resources/svg/star.svg"></img>
+    <img src="../resources/svg/logo.svg"></img>
     <a href="."><h1>Itsakon Hotel</h1></a>
     <section class=stars>
-        <?php for ($i=0; $i < $stars; $i++) { 
+        <?php for ($i=0; $i < $stars; $i++) {
             ?><img src="../resources/svg/star.svg"></img><?php
         }?>
     </section>
